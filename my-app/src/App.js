400ec5b70335ad683274a4744a5d2f9e69cac5d3
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-08-29 13:17:42 +0500

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hellow World</h1>
+       <div>
+        <h1>hellow world</h1>
+       </div>
 
       </header>
     </div>
