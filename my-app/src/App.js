3d1c77234422d6b3bdbdb9233d2c8174d26d565d
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-09-03 16:11:01 +0500

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,9 @@
-
-import './App.css';
-
-function Profile() {
+export default function Profile() {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
+      src="https://i.imgur.com/MK3eW3Am.jpg"
       alt="Katherine Johnson"
     />
-  );
-}
-
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
+  )
 }
 
