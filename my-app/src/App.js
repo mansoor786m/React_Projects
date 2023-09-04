77ddb9ab8c56c989415094f2572d962958e9a12d
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-09-04 16:20:21 +0500

    function in a function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,20 @@
-export default function Profile() {
+function Profile() {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3Am.jpg"
+      src="https://i.imgur.com/MK3eW3As.jpg"
       alt="Katherine Johnson"
     />
-  )
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
 }
 
