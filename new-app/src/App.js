56d6b36fdf3d54f64b4b3b5052cdf8d75ef73854
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-09-16 20:43:16 +0500

    update

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 import './App.css'
-// const Array = [mansoor , tayyab , nadeem ]
 
 
-const Obj = {
-  name: "mansoor" ,
-  age: 30
 
-}
+// const Obj = {
+//   name: "mansoor" ,
+//   age: 30
+
+// }
 
 
-function Diff() {
-  return (
-  <>
-    <h1>my name is Muhammad {Obj.name} and my ageeeeeeeeeee is {Obj.age} year</h1>
+// function Diff() {
+//   return (
+//   <>
+//     <h1>my name is Muhammad {Obj.name} and my ageeeeeeeeeee is {Obj.age} year</h1>
 
-  </>
-  );
-}
+//   </>
+//   );
+// }
 
 
 const Change = () => {
@@ -27,7 +27,7 @@ const Change = () => {
   return (
     <>
       <h1>Hellow World </h1>
-      {/* <Diff  /> */}
+    
 
 
 
