a75d9abfdd5b46890de97617f6e3209d45706476
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-09-16 18:32:44 +0500

    practice

---
+++ 01 count Down React/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+// import ReactDOM from 'react-dom/client';
+import './App.css';
+
+
+
+// function showDate(d) {
+//   return (
+//     <h2>{nowDate}</h2>
+//   )
+// }
+
+const Change = () => {
+
+
+  const nowDate = new Date();
+  console.log(nowDate.toString());
+
+
+
+  return (
+    <>
+      <h1>React Project NO 01 </h1>
+      <h1>Count Down App</h1>
+      <h2> {nowDate.toString()}</h2>
+      <input type = "date" placeholder="enter date"/>
+
+
+
+    </>
+  );
+}
+
+
+export default Change;
+
+
+// const today = new Date();
+
+// function formatDate(date) {
+//   return new Intl.DateTimeFormat(
+//     'en-US',
+//     { weekday: 'long' }
+//   ).format(date);
+// }
+
+// export default function TodoList() {
+//   return (
+//     <h1>To Do List for {formatDate(today)}</h1>
+//   );
+// }
---
+++ new-app/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import './App.css'
+
+function Diff() {
+  return (
+  <>
+    <h1> my app</h1>
+  </>
+  );
+}
+
+
+const Change = () => {
+
+
+
+
+
+
+  return (
+    <>
+      <h1>Hellow World </h1>
+      <Diff />
+
+
+
+    </>
+  );
+}
+
+
+export default Change;
