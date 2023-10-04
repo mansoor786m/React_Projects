d2f4fe1d7a5fe8e218cac4ce6c12777cbcffd8ea
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-10-04 13:21:02 +0500

    start cv form

---
+++ 00 React CV form/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function Show({object}){
+    return(
+        <div>
+        <h1> my name is {object.name} and my age is {object.age}</h1>
+        </div>
+    );
+}
+
+export default function App(){
+
+    
+    return(
+        <div>
+            <Show object = {{    name: 'mansoor', age: 30 }} />
+        </div>
+    )
+}
