2b3dabe915bef7f4f3f3322ca4b8829a23a7ffb0
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-10-13 10:11:42 +0500

    02textutility created and navbar added

--- 00 React CV form/src/App.js
+++ 00 React CV form/src/App.js
@@ -6,11 +6,9 @@ export default function App() {
   return (
     <div className="main-page">
       <Section>
-        djhgfsdjfmy name is mansoor and `my father nadagdau`
+       
       </Section>
-      <section>
-        jfgsdsdfdufksdfj
-      </section>
+
 
 
     </div>
---
+++ 02textutility/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar title="Textutility" about='Disable'/>
+    </>
+  );
+}
+
+export default App;
