e0457fff12a203249681395556a6c358843f2a3e
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-10-26 18:34:36 +0500

    dark mode

--- 02textutility/src/App.js
+++ 02textutility/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import './App.css';
 import About from './component/About';
 import Navbar from './component/Navbar';
 import Textarea from './component/Textarea';
 
 function App() {
+  const [mode, setmode] = useState('light')
+  const handleDarkMode = () => {
+    
+    if (mode=== 'light') {
+      setmode('dark')
+    }else{
+      setmode('light')
+    }
+
+  }
+
   return (
     <>
-      <Navbar title="Textutility" about='Disable'/>
-      <Textarea title="Enter Your Text Here :"></Textarea>
-      <About/>
+      <Navbar title="Textutility" about='Disable' mode = {mode} toggleMode={handleDarkMode} />
+      <Textarea title="Enter Your Text Here :" mode ={mode} ></Textarea>
+      <About />
     </>
   );
 }
--- 02textutility/src/component/Navbar.js
+++ 02textutility/src/component/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <div className='container my-3'>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className={`navbar navbar-expand-lg bg-${props.mode}  `}>
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,6 +39,10 @@ export default function Navbar(props) {
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
+                    <div className={`form-check mx-3 form-switch text-${props.mode=== 'light'? 'black':'white'}`}>
+                            <input className="form-check-input" type="checkbox" role="switch" id="dark-mode" onClick={props.toggleMode} />
+                            <label className="form-check-label" htmlFor="dark-mode">Enable Dark mode</label>
+                        </div>
                 </div>
             </nav>
         </div>
