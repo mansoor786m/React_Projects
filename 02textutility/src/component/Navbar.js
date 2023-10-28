15c4485173ddfa42058daae9667e51bd57a25132
Muhammad Mansoor <mansoorjbd@gmail.com>
2023-10-28 18:44:31 +0500

    Update Navbar.js

--- 02textutility/src/component/Navbar.js
+++ 02textutility/src/component/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <div className='container my-3'>
-            <nav className={`navbar navbar-expand-lg bg-${props.mode}  `}>
+            <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}  `}>
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
