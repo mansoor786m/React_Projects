247ffa532de57309feeac546d282f02de096b90e
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-01-07 09:44:32 +0500

    update

--- 02textutility/src/App.js
+++ 02textutility/src/App.js
@@ -6,34 +6,55 @@ import Navbar from './component/Navbar';
 import Textarea from './component/Textarea';
 import Alert from './component/Alert';
 
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   const [mode, setmode] = useState('light');
   const [alert, setAlert] = useState(null);
   const handleDarkMode = (e) => {
     setmode(e);
 
-    if (e==='light') {
+
+    if (e === 'light') {
       console.log(e)
       document.body.style.backgroundColor = '#ecf0f1';
       showAlert("Light mode is actived", "success");
-      
+      setInterval(() => {
+        document.title = "Textutils -Home"
+      }, 2000);
+      setInterval(() => {
+        document.title = "Textutils -Light"
+      }, 1500);
     }
-    if (e==='grey') {
+    if (e === 'grey') {
       console.log(e)
       document.body.style.backgroundColor = 'grey';
       showAlert("Dark mode is actived", "success");
+      setInterval(() => {
+        document.title = "Textutils -Home"
+      }, 2000);
+      setInterval(() => {
+        document.title = "Textutils -Grey"
+      }, 1500);
     }
-    if (e==='blue') {
+    if (e === 'blue') {
       console.log(e)
       document.body.style.backgroundColor = '#3498db';
       showAlert("Blue mode is actived", "success");
     }
-    if (e==='green') {
+    if (e === 'green') {
       console.log(e)
       document.body.style.backgroundColor = '#27ae60';
       showAlert("Green mode is actived", "success");
+
     }
-     
+
 
   }
 
@@ -49,10 +70,23 @@ function App() {
 
   return (
     <>
+      
+      <Router>
       <Navbar title="Textutility" about='Disable' mode={mode} handleDarkMode={handleDarkMode} />
       <Alert alert={alert} />
-      <Textarea showAlert={showAlert} title="Enter Your Text Here :" mode={mode} ></Textarea>
-      <About />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/">
+            <Textarea showAlert={showAlert} title="Enter Your Text Here :" mode={mode} ></Textarea>
+          </Route>
+        </Switch>
+      </Router>
+
+
+
     </>
   );
 }
--- 02textutility/src/component/Navbar.js
+++ 02textutility/src/component/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import '../index.css'
+import { Link } from 'react-router-dom';
 
 
 export default function Navbar(props) {
@@ -9,11 +10,11 @@ export default function Navbar(props) {
     const handleclick = (color) => {
         console.log(radioCheck)
         // console.log(color)
-        if( color==='blue' || color==='green' && radioCheck ===false ){
+        if(( color==='blue' || color==='green') && radioCheck ===false ){
             props.handleDarkMode(color)
 
         }
-        if(color==='blue' || color==='green' && radioCheck === true ){
+        if((color==='blue' || color==='green' )&& radioCheck === true ){
             props.handleDarkMode(color)
             setradioCheck(false)
         }else{
@@ -39,17 +40,17 @@ export default function Navbar(props) {
         <div className="container my-3  ">
             <nav className={`navbar navbar-expand-lg navbar-${props.mode}  bg-${props.mode === 'light' ? '#95a5a6' : 'grey'}  `}>
                 <div className={`container-fluid `}>
-                    <a className={`navbar-brand  text-${props.mode === 'light' ? 'black' : 'white'}`} href="/">{props.title}</a>
+                    <Link className={`navbar-brand  text-${props.mode === 'light' ? 'black' : 'white'}`} to="/">{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className={`collapse navbar-collapse text-${props.mode === 'light' ? 'grey' : 'white'}`} id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
+                                <Link className="nav-link active" aria-current="page" to="/">{props.home}</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">About</a>
+                                <Link className="nav-link" to="/about">About</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
