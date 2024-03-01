635147639e3c3dfd2580ce02c55ec787ad4e27a6
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-03-01 12:43:04 +0500

    count down work

--- 01 count Down React/src/App.js
+++ 01 count Down React/src/App.js
@@ -14,16 +14,22 @@ const Change = () => {
 
 
   const nowDate = new Date();
-  console.log(nowDate.toString());
+  // console.log(nowDate.toString());
+
 
 
 
   return (
     <div className="body">
-      <h1>React Project NO 01 </h1>
-      <h1>Count Down App</h1>
-      <h2> {nowDate.toString()}</h2>
-      <input type = "date" placeholder="enter date"/>
+      <h1  className="text-4xl font-bold underline my-3">React Project NO 01 </h1>
+      <h1  className="text-2xl font-bold">Count Down App</h1>
+      <h2  className="text-3xl text-green-500 my-4 font-bold underline"> {nowDate.toString()}</h2>
+      <input  className="text-2xl text-blue-600 my-4 font-bold " type = "date" placeholder="enter date"/>
+
+      <button className='text-center text-2xl bg-gray-500 font-bold rounded-xl p-2 m-3'>
+      Get Started
+    </button>
+
 
 
 
