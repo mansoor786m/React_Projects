8af361bd749bca259166375e9e19134734103316
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-03-13 12:21:50 +0500

    Update App.js

--- 01 count Down React/src/App.js
+++ 01 count Down React/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import ReactDOM from 'react-dom/client';
 import './App.css';
 
@@ -10,27 +10,46 @@ import './App.css';
 //   )
 // }
 
+
 const Change = () => {
 
 
-  const nowDate = new Date();
-  // console.log(nowDate.toString());
+
+  const [time, setTime] = useState(new Date())
+  const [finalDate, setFinaldate] = useState()
+
+
+
+
+
+
+  console.log(time);
+  // // const [target, setTarget] = useState(nowDate.toLocaleString())
+
+  const handleClick = () => {
+    //   console.log(nowDate.toLocaleString())
+    //   console.log(nowDate.to)
+  }
+
+  const HandleChange = (e) => {
+
+    setFinaldate(new Date())
+  }
 
 
 
 
   return (
     <div className="body">
-      <h1  className="text-4xl font-bold underline my-3">React Project NO 01 </h1>
-      <h1  className="text-2xl font-bold">Count Down App</h1>
-      <h2  className="text-3xl text-green-500 my-4 font-bold underline"> {nowDate.toString()}</h2>
-      <input  className="text-2xl text-blue-600 my-4 font-bold " type = "date" placeholder="enter date"/>
+      <h1 className="text-4xl font-bold underline my-3">React Project NO 01 </h1>
+      <h1 className="text-2xl font-bold">Count Down App</h1>
+      <h2 className="text-3xl text-green-500 my-4 font-bold underline"> {time.toString()}</h2>
 
-      <button className='text-center text-2xl bg-gray-500 font-bold rounded-xl p-2 m-3'>
-      Get Started
-    </button>
+      <input className="text-2xl text-blue-600 my-4 font-bold " type="datetime-local" placeholder="enter date" onChange={HandleChange} />
 
-
+      <button className='text-center text-black text-2xl bg-green-500 rounded-full font-extrabold  p-2 m-3' onClick={handleClick}>
+        Get Started
+      </button>
 
 
     </div>
