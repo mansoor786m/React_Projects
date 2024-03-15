3fa510a1eb9f4a379482e33ca8892d5d3b914570
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-03-15 11:25:48 +0500

    finao update

--- 01 count Down React/src/DateTimeDisplay.jsx
+++ 01 count Down React/src/DateTimeDisplay.jsx
@@ -4,23 +4,23 @@ export default function DateTimeDisplay({month ,day ,hrs,min, sec}) {
   return (
     <div className='flex flex-row m-10'>
         <div className='flex flex-col m-2 '>
-            <input className='text-black font-bold'>{month}</input>
+            <input className='text-black font-bold' value={month}/>
             <label className='text-red-100 font-bold'>Month</label>
         </div>
         <div className='flex flex-col m-2'>
-            <input className='text-black font-bold'>{day}</input>
+            <input className='text-black font-bold'Value={day}/>
             <label className='text-red-100 font-bold'>day</label>
         </div>
         <div className='flex flex-col m-2 '>
-            <input className='text-black font-bold'>{hrs}</input>
+            <input className='text-black font-bold'value ={hrs}/>
             <label className='text-red-100 font-bold'>Hrs</label>
         </div>
         <div className='flex flex-col m-2'>
-            <input className='text-black font-bold'>{min}</input>
+            <input className='text-black font-bold'value={min}/>
             <label className='text-red-100 font-bold'>Min</label>
         </div>
         <div className='flex flex-col m-2'>
-            <input className='text-black font-bold'>{sec}</input>
+            <input className='text-black font-bold'value={sec}/>
             <label className='text-red-100 font-bold'>Sec</label>
         </div>
       
