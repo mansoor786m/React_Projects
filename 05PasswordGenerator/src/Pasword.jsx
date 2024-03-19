c3c23d263ef23b3a2e9e1ecca394c1f6fbbe715a
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-03-19 12:39:28 +0500

    Update Pasword.jsx

--- 05PasswordGenerator/src/Pasword.jsx
+++ 05PasswordGenerator/src/Pasword.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Pasword() {
-    const [password , setPassword] = useState(null)
-    const [characterAllowed , setCharacterAllowed] =useState(false)
+    const [password , setPassword] = useState("")
+    const [charAllowed , setCharAllowed] =useState(false)
     const [numberAllowed , setNumberAllowed] = useState(false)
-    const [lengh , setLength] = useState(8)
+    const [length , setLength] = useState(8)
 
 
-    const PaswordGenerator =()=>{
+    const passwordGenerator =()=>{
         let pass = ""
         let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        if(characterAllowed) str += "0123456789"
+        if(charAllowed) str += "0123456789"
         if(numberAllowed)  str += "!@#$%^&*-_+=[]{}~`"
-        for (let index = 0; index < srt.length; index++) {
-            const num = Math.floor(math.random())*str.length +1 ;
+        for (let i = 1; i <=length; i++) {
+            const num = Math.floor(Math.random()*str.length +1 );
             pass += str.charAt(num)
-            console.log(pass)
+            
             
         }
-
+        setPassword(pass)
+        console.log(pass)
     }
 
-    useEffect(()=>{
-        PaswordGenerator()
-    },[lengh,numberAllowed, characterAllowed, PaswordGenerator ]
-    )
+    useEffect(() => {
+        passwordGenerator()
+      }, [length, numberAllowed, charAllowed , ])
 
 
 
@@ -46,9 +46,44 @@ export default function Pasword() {
             >copy</button>        
         </div>
 
+        <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={40}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
 
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
         
-     
+     </div>
     </div>
   )
 }
