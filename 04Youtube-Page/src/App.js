69c80a11261cdf75b2c048d124bffcd6dc7f3517
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-06-01 05:29:48 +0500

    created

--- 04Youtube-Page/src/App.js
+++ 04Youtube-Page/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import './App.css';
+import Video from './components/Video';
 
-export default function App() {
+function App() {
+  let obj = {
+    title: 'React JS tutorial',
+    views: '999K',
+    time: '1 year ago',
+    channel: 'Coder Dost',
+  };
   return (
-    <div>
-      Hellow
+    <div className="App">
+
+      <Video {...obj}></Video>
+      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video
+        title="Mongo DB tutorial"
+        views="1M"
+        time="1 month ago"
+        channel="Coder Dost"
+      ></Video>
     </div>
-  )
+  );
 }
+
+export default App;
