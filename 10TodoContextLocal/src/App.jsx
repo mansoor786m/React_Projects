c6309f86d0a292362f25e46bfe8db72e6f2116ac
Muhammad Mansoor <mansoorjbd@gmail.com>
2024-07-20 23:28:22 +0500

    useeffect insert

--- 10TodoContextLocal/src/App.jsx
+++ 10TodoContextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import { TodoProvider } from './contexts'
@@ -22,6 +22,18 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
 
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+
+  }, [todos])
 
   return (
     <TodoProvider value={{ todos, addTodo, deleteTodo, toggleComplete, updatedTodo }}>
